7a1b00b5898983351d3058372ddd24a143aa4d45
Vicky Kumar <kumar.vickysah72@gmail.com>
2023-07-27 21:51:01 +0530

    Added Basic set for Booking Service

---
+++ src/controllers/info-controller.js
@@ -0,0 +1,12 @@
+//const {statusCode}=require('http-status-codes');
+
+const info = (req,res)=>{
+    return res.status(500).json({
+        sucess:true,
+        message:'API is live',
+        error:{},
+        data:{}
+    });
+}
+
+module.exports={info};
---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express= require('express');
+const {ServerConfig,logger} = require('./config');
+const apiroutes=require('./routes');
+
+
+const app = express();
+app.use('/api',apiroutes);
+
+
+app.listen(ServerConfig.PORT,()=>{
+    console.log(`Sucessfully started the server at ${ServerConfig.PORT}`);
+    // logger.info("sucessfully started the server",{});
+});
